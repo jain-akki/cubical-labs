200625efa6f0493d5341b535c8b5393dcef97210
Akshay Jain <jain.akshay5244@gmail.com>
2017-04-19 01:57:34 +0530

    minor changes!!

--- js/app.js
+++ js/app.js
@@ -25,21 +25,21 @@
 
     $scope.getArtist = function (art) {
       if (art !== undefined) {
-        console.log(art);
+        console.log('artist object: ', art);
         if (art.name.toLowerCase() === "jack" && art.limit === 4) {
           $http({
             method: 'GET',
             contentType: 'application/jsonp',
             url: baseUrl + "search?term=" + art.name + "&limit=" + art.limit
           }).then(function (res) {
-            console.log('----', res.data.results);
+            console.log('res: ', res.data.results);
             $scope.data = res.data.results;
           });
           $('#modal1').modal('close');
           $scope.searchBtn = false;
           $scope.tabs = true;
         } else {
-          alert('Track Name : Jack\nNo. of Tracks : 4');
+          alert('Artist Name : Jack\nNo. of Tracks : 4');
         }
         art = {};
       } else {
